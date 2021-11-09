47857b9d1877203ca66049a102d5101e10279c66
goingdust <dustinluck@gmail.com>
2021-11-09 19:14:17 +0000

    wrote a function to find out which venues would be viable voting stations for an election

---
+++ voting-station.js
@@ -0,0 +1,25 @@
+// must have a capacity of at least 20 and be a school or community centre
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations(stations) {
+  const goodStations = [];
+  
+  for (const station of stations) {
+    const capacity = station[1];
+    const type = station[2];
+
+    if (capacity >= 20) {
+      if (type === 'school' || type === 'community centre') {
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+}
+
+console.log(chooseStations(stations));
