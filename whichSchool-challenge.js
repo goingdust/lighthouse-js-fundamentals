e14fc0ccc256a6a66dad6650c3ae7425adcd955a
goingdust <dustinluck@gmail.com>
2021-11-09 03:02:41 +0000

    solved the JuliaJames (FizzBuzz) Loop challenge

--- whichSchool-challenge.js
+++ whichSchool-challenge.js
@@ -9,3 +9,20 @@ const whichSchool = function (age) {
 }
 
 console.log(whichSchool(100));
+
+// JuliaJames
+
+var x = 1;
+
+while (x <= 20) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    console.log('JuliaJames');
+  } else if (x % 3 === 0) {
+    console.log('Julia');
+  } else if (x % 5 === 0) {
+    console.log('James');
+  } else {
+    console.log(x);
+  }
+  x++;
+}
